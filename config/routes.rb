f7863d51f7fbbb79b4cd0664eba73d7509954b94
Christopher-Edynak <christopher.edynak@outlook.com>
2021-08-18 15:45:05 -0400

    implemented seperate root path for authenticated users

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
 
   root 'welcome#index'
-  
+
   devise_for :users
 
   get 'welcome/index'
+  authenticated :user do
+    root "documents#index", as: "authenticated_root"
+  end
 
   resources :documents
   
