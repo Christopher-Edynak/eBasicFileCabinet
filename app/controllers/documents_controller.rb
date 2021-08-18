72747359f8e3e28b1d72a4ebd1454538653e384f
Christopher-Edynak <christopher.edynak@outlook.com>
2021-08-18 14:46:55 -0400

    implemted view file for document model

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -7,9 +7,17 @@ class DocumentsController < ApplicationController
   end
 
   def new
+    @document = Document.new
   end
 
   def create
+    @document = Document.new(document_params)
+
+    if @document.save
+      redirect_to @document
+    else
+      render 'new'
+    end
   end
 
   def edit
@@ -27,5 +35,6 @@ class DocumentsController < ApplicationController
     end
 
     def document_params
+      params.require(:document).permit(:title, :content)
     end
 end
