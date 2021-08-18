659399ec72904ad030584f5fef7407e3166c60a1
Christopher-Edynak <christopher.edynak@outlook.com>
2021-08-18 15:35:46 -0400

    implemented association between user and files

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_18_192326) do
+ActiveRecord::Schema.define(version: 2021_08_18_192732) do
 
   create_table "documents", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
